bd5ac26b87b7f41811e9ad7d8a6c66b3c7d9a59c
Dave Raring <d@7pi.co>
2023-02-24 20:07:52 -0600

    fixing default ownership for bucket module

--- modules/terraform-state/_variables.tf
+++ modules/terraform-state/_variables.tf
@@ -90,7 +90,7 @@ variable "s3_replication_source_roles" {
 
 variable "s3_object_ownership" {
   type        = string
-  default     = "BucketOwnerPreferred"
+  default     = "BucketOwnerEnforced"
   description = "Specifies the S3 object ownership control. Valid values are `ObjectWriter`, `BucketOwnerPreferred`, and 'BucketOwnerEnforced'."
 }
 
